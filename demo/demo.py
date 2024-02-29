3b3306656b721d7f3ffba045b47eaa7e781a7618
AlexandreGirold <killersgg78@gmail.com>
2024-02-29 10:16:06 +0530

    positions recognition

---
+++ demo/demo.py
@@ -0,0 +1,21 @@
+import cv2
+import utils
+import time
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+h, w, c = cap.read()[1].shape
+
+pose = utils.Pose()
+screen = utils.Screen(w, h)
+
+
+while True:
+    success, img = cap.read()
+
+    pose_landmarks, img = pose.findPose(img, draw=screen.buttons[0].state)
+    screen.draw(img, pose_landmarks)
+
+    img = cv2.resize(img, (w*2, h*2))
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
