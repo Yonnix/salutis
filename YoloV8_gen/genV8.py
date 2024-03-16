a0157b5d1b0a7bb9b979ba2cfdf6da878e79abe3
AlexandreGirold <killersgg78@gmail.com>
2024-03-16 19:06:51 +0530

    Generation of YOLOv8 data

--- YoloV8_gen/genV8.py
+++ YoloV8_gen/genV8.py
@@ -3,20 +3,29 @@
 import sys
 import numpy as np
 import cv2
+import os
 
 
-def open_image(image_path):
-    image = cv2.imread(image_path)
-    cv2.imshow('image', image)
-    return image
+
+def open_images(image_folder_path):
+    for image in os.listdir(image_folder_path):
+        image_path = os.path.join(image_folder_path, image)
+        img = cv2.imread(image_path)
+        yield image_path ,img 
+        while True:
+            k = cv2.waitKey(0) & 0xFF
+            if k == ord(' '):  # space key
+                break
 
 class Coordinates:
     
-    def __init__(self, image):
+    def __init__(self, image, file_name):
         self.image = image
         self.height, self.width, self.channels = image.shape
         self.click_coord = []
-        self.should_close = False 
+        self.string = ""
+        self.file_name = file_name
+        # self.should_close = False 
 
 
     def click_event(self, event, x, y, flags, param):
@@ -26,38 +35,64 @@ class Coordinates:
             if len(self.click_coord) == 4:
                 print(self.click_coord)
                 print("Middle: ", self.middle())
+                print("Height and Width: ", self.hight_width())
+                self.yoloV8_txt()
                 self.click_coord = []  # Reset the list for next four clicks
-                self.should_close = True
+                # self.should_close = True
+
 
     def middle(self):
         height, width, _ = self.height, self.width, self.channels
-        x = (self.click_coord[0][0] + self.click_coord[2][0]) / 2
-        y = (self.click_coord[1][1] + self.click_coord[3][1]) / 2
+        X = (self.click_coord[0][0] + self.click_coord[2][0]) / 2
+        Y = (self.click_coord[1][1] + self.click_coord[3][1]) / 2
 
         # Normalize the values
-        x = x / width
-        y = y / height
+        X = X / width
+        Y = Y / height
 
-        print(x, ' ', y)
-
-        return x, y
-
-def yoloV8_txt(image, x, y):
-    string = "0 f{x} f{y} " # 0 is the class human
+        return X, Y
     
+
+    def hight_width(self):
+        height, width, _ = self.height, self.width, self.channels
+        h = self.click_coord[3][1] - self.click_coord[1][1] # Second and fourth click
+        w = self.click_coord[2][0] - self.click_coord[0][0] # First and third click
+
+        # Normalize the values
+        h = h / height
+        w = w / width
+
+        return h, w
+    
+
+    def yoloV8_txt(self):
+        X, Y = Coordinates.middle(self)
+        h, w = Coordinates.hight_width(self)
+        self.string = "0 " + str(X) + " " + str(Y) + " " + str(h) + " " + str(w) + "\n"
+        with open(self.file_name + ".txt", "a") as file:
+            file.write(self.string)
+    
+
 def main():
-    image_path = "/home/alex/salutis/business_canvas_model.png"
-    image = open_image(image_path)
-    coordinates = Coordinates(image)
-    cv2.setMouseCallback('image', coordinates.click_event)
-    
+    images_path = sys.argv[1] # Path in the command line to the images
+    images = open_images(images_path)
+    # for image in images:
+    #     cv2.imshow("test", image)
     try:
-        while True:
-            if coordinates.should_close:  # Check the flag to see if the window should be closed
-                cv2.destroyAllWindows()
-                break
-            if cv2.waitKey(1) & 0xFF == 27:  # ASCII value of ESC is 27
-                break
+        for image_name, image in images:
+            # Create .txt file with the same name as the image
+            file_name = image_name.split("/")[-1].split(".jpg")[0] + ".txt"
+            print(file_name)
+            cv2.imshow('image', image)
+            coordinates = Coordinates(image, file_name)
+            is_closed = False
+            while not is_closed:
+                cv2.imshow('image', image)
+                cv2.setMouseCallback('image', coordinates.click_event)
+                k = cv2.waitKey(1) & 0xFF  # Non-blocking key check
+                if k == ord(' '):  # space key
+                    is_closed = True
+            cv2.waitKey(1)  # Ensure the window is destroyed before the next image is displayed
     except KeyboardInterrupt:
         print("\n Interrupted by user")
 
